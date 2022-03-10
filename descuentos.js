5e8a2df369e18b8d32f841fa5d880e6ffed7ac7a
Juan Llanos <judallade@gmail.com>
2022-03-10 00:33:56 -0600

    Se agrego la función para que trabaje la interactividad del JS con el HTML.

--- descuentos.js
+++ descuentos.js
@@ -1,18 +1,19 @@
-// const precioOriginal = 120;
-// const descuento = 18;
-
-function calcularPrecioConDescuento(precio, descuento){
+function calcularPrecioConDescuento(precio, descuento) {
     const porcentajePrecioConDescuento = 100 - descuento;
     const precioConDescuento = (precio *  porcentajePrecioConDescuento) / 100;
 
     return precioConDescuento;
 }
 
+function onClickButtonPriceDiscount() {
+    const inputPrice = document.getElementById("InputPrice");
+    const priceValue = inputPrice.value;
 
+    const inputDiscount = document.getElementById("InputDiscount");
+    const discountValue = inputDiscount.value;
 
-// console.log({
-//     precioOriginal,
-//     descuento,
-//     porcentajePrecioConDescuento,
-//     precioConDescuento,
-// });
+    const precioConDescuento = calcularPrecioConDescuento(priceValue, discountValue);
+
+    const resultP = document.getElementById("ResultP");
+    resultP.innerText = "El precio con descuento es $" + precioConDescuento;
+}
